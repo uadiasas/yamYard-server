5b5390e3a08e9fa9d43b48832e1a82b8fce08d80
Vadim <uadiasim@gmail.com>
2024-04-12 16:55:43 +0300

    delete main.py

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+dfgsfgsdfg
+sdfg
+sdfg
+sdfg
+s
+dfg
+sdfgsd
+fg
+sd
+fg
+sdfgs
+dgf
