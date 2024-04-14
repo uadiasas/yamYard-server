e7fbb5b6151a3e08b21cb10b65da19c949a1befb
Vadim <uadiasim@gmail.com>
2024-04-15 00:52:13 +0300

    Обновление настроек

--- yamYard/api/urls.py
+++ yamYard/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('v1/users/', views.UserList.as_view()),
+    path('v1/users/<int:pk>/', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
