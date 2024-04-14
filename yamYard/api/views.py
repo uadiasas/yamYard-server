8c563be7997b3d74d2ce788f1a532970a4156efb
Vadim <uadiasim@gmail.com>
2024-04-14 18:24:49 +0300

    Добавление сериализаторов api, списка всех пользователей и деталей к каждому пользователю

--- yamYard/api/views.py
+++ yamYard/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
