9d58b364464f0187f68920a96dc0779d7b160fee
Vadim <uadiasim@gmail.com>
2024-04-15 17:41:18 +0300

    Изменил название проекта на правильное

--- yumYard/yumYard/asgi.py
+++ yumYard/yumYard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for yamYard project.
+ASGI config for yumYard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yamYard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yumYard.settings')
 
 application = get_asgi_application()
