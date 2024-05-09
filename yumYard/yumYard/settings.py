b699f62bc2fc22b4f591bc2e3fb30d98c2295888
Vadim <uadiasim@gmail.com>
2024-05-09 09:50:33 +0300

    добавил сваггер

--- yumYard/yumYard/settings.py
+++ yumYard/yumYard/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework.authtoken',
     'djoser',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -140,5 +141,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
