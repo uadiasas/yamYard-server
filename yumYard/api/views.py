a41ac000875ba94c4656af5a626a80334e9654d8
Vadim <uadiasim@gmail.com>
2024-06-25 01:53:59 +0300

    добавил возможность обычным пользователям получать список категорий

--- yumYard/api/views.py
+++ yumYard/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django_rest.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import generics, permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
